b66d2f8bb654fdf029028be02e9f2d3cb2f23d8c
EduardAlex <edyalex.david@gmail.com>
2020-06-05 22:50:41 +0300

    added control by post requests

--- app.py
+++ app.py
@@ -10,10 +10,13 @@ languages = ["romanian/Romanian/Polly.Carmen/ro-RO",
 	"english-uk/English (UK)/Polli.Amy/en-GB",
 	"russian/Russian/Polli.Tatyana/ru-RU"]
 langs = {}
-#with open("langs.txt", "r") as f:
+
 for line in languages:
 	p = line.split("/")
 	langs[p[0]] = [p[1],p[2],p[3]]
+
+on = True
+switchpass = "iksworbadzsram"
 
 @app.route("/")
 def index():
@@ -21,25 +24,34 @@ def index():
 
 @app.route("/call")
 def callpage():
-	return render_template("call.html", langs = langs)
-
-@app.route("/call", methods=['POST'])
-def call():
-	sid = os.environ.get("SID_T")
-	token = os.environ.get("TOKEN_T")
-	client = Client(sid, token)
-	number = request.form['number']
-	text = request.form['text']
-	language = request.form['language']
-	loop = request.form['loop']
-	twimlcall = VoiceResponse()
-	if request.form['mp3orvoice'] == "voice":
-		twimlcall.say(text, voice=langs[language][1], language=langs[language][2])
+	if on:
+		return render_template("call.html", langs = langs)
 	else:
-		twimlcall.play(text, loop = loop)
-	print(str(twimlcall))
-	call = client.calls.create(from_ = "+12055832852", to = number, twiml = str(twimlcall))
+		return render_template("closed.html")
+
+@app.route("/calling", methods=['POST'])
+def call():
+	if request.method == 'POST':
+		sid = os.environ.get("SID_T")
+		token = os.environ.get("TOKEN_T")
+		client = Client(sid, token)
+		number = request.form['number']
+		text = request.form['text']
+		language = request.form['language']
+		loop = request.form['loop']
+		twimlcall = VoiceResponse()
+		if request.form['mp3orvoice'] == "voice":
+			twimlcall.say(text, voice=langs[language][1], language=langs[language][2])
+		else:
+			twimlcall.play(text, loop = loop)
+		print(str(twimlcall))
+		call = client.calls.create(from_ = "+12055832852", to = number, twiml = str(twimlcall))
 	return redirect("/call")
+
+@app.route("/switch", methods=['POST'])
+def switchc():
+	if request.form['password'] == switchpass:
+		on = (not on)
 
 if __name__ == '__main__':
 	app.run(debug = 1, host="0.0.0.0", port=80)
